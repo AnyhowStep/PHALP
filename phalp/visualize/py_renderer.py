2f51510bc0d1b7efd08f6b636161b60a28063b27
Justin Ng <5655961+AnyhowStep@users.noreply.github.com>
2025-12-26 22:36:04 -0500

    remove debug print

--- phalp/visualize/py_renderer.py
+++ phalp/visualize/py_renderer.py
@@ -148,7 +148,6 @@ class Renderer:
         
         color, rend_depth = self.renderer.render(scene, flags=pyrender.RenderFlags.RGBA)
         self.renderer.delete()
-        print("delete renderer")
         color = color.astype(np.float32) / 255.0
         
         return color
