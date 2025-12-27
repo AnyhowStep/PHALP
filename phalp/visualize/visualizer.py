7878bc8457e069dd8be98bfe90d612ac1705dce5
Justin Ng <5655961+AnyhowStep@users.noreply.github.com>
2025-12-27 01:52:26 -0500

    convert joints to list

--- phalp/visualize/visualizer.py
+++ phalp/visualize/visualizer.py
@@ -375,7 +375,8 @@ class Visualizer(nn.Module):
                     rendered_image_final = rendered_image_final[:, :, top_:top_+img_height_, left_:left_+img_width_]
 
                     final_visuals_dic.setdefault("smpl_joints", {})
-                    final_visuals_dic["smpl_joints"][ids_x] = joints
+                    print(type(ids_x))
+                    final_visuals_dic["smpl_joints"][ids_x] = joints.tolist()
 
                 if "MASK" in self.cfg.render.type or "BBOX" in self.cfg.render.type:
                     seg_mask = tracked_mask[ids_x]
